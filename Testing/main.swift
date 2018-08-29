371210a10a00985eb6a658a0b989a3b1c8dd379c
luissantiagodev <luissantiagodev@gmail.com>
2018-08-29 16:46:46 -0500

    Initial Commit

---
+++ Testing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Testing
+//
+//  Created by Legendary Wicho on 8/29/18.
+//  Copyright © 2018 Luis Santiago. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
